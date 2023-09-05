f7922fcfb138989dfa181a5e59c2bb6a5f2241e6
jabber <2898117012@qq.com>
2023-09-05 19:50:23 +0800

    init

--- se-block.php
+++ se-block.php
@@ -16,3 +16,28 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+if ( ! function_exists( 'se_block_activation' ) ) {
+	function se_block_activation() {
+
+	}
+}
+
+register_activation_hook( 'se_block_activation' );
+
+if ( ! function_exists( 'se_block_deactivation' ) ) {
+	function se_block_deactivation() {
+
+	}
+}
+
+register_deactivation_hook( 'se_block_deactivation' );
+
+if ( ! function_exists( 'se_block_load_textdomain' ) ) {
+	function se_block_load_textdomain() {
+		load_plugin_textdomain( 'se_block', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	}
+}
+
+add_action( 'init', 'se_block_load_textdomain' );
+
